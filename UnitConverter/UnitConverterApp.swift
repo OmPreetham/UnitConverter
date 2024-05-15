6d0698acdd68d754eda5c44cbf8ba9cdc93c7099
Om Preetham <ompreethambandi@gmail.com>
2024-05-14 22:11:55 -0500

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Om Preetham Bandi on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
